066b6664098194640a2fbf05a93e2ae9821d6b00
RQ2OL <56695777+LoloLeMono@users.noreply.github.com>
2022-11-13 19:43:07 +0100

    Add TP_SOAP clean

---
+++ HAI704I_ArchiLog/SOAP/hotel.web.service.client/src/main/java/hotel/web/service/client/ReserverResponse.java
@@ -0,0 +1,62 @@
+
+package hotel.web.service.client;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour reserverResponse complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType name="reserverResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="return" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "reserverResponse", propOrder = {
+    "_return"
+})
+public class ReserverResponse {
+
+    @XmlElement(name = "return")
+    protected String _return;
+
+    /**
+     * Obtient la valeur de la propriété return.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getReturn() {
+        return _return;
+    }
+
+    /**
+     * Définit la valeur de la propriété return.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setReturn(String value) {
+        this._return = value;
+    }
+
+}
