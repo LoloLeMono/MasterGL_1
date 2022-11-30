3a61bb7a1a7e1aa1f4821ccf84adf7654265ea5b
LoloLeMono <rouquairolr@gmail.com>
2022-11-30 08:54:29 +0100

    Ajout calculette-prefixe

---
+++ HAI712I_IngeLog/TP8/calculette-prefixe/src/calculettePrefixe/CalculetteException.java
@@ -0,0 +1,11 @@
+package calculettePrefixe;
+
+class CalculetteException extends RuntimeException
+{
+	static final long serialVersionUID = 0;
+	
+	public CalculetteException(String s)
+	{
+		super(s);
+	}
+}
---
+++ HAI712I_IngeLog/TP8/calculette-prefixe/src/calculettePrefixe/ENombre1.java
@@ -0,0 +1,24 @@
+package calculettePrefixe;
+
+public class ENombre1 extends EtatCalculette
+{
+
+	ENombre1(Calculette c)
+	{
+		super(c);
+	}
+	
+	public int enter(String s) throws CalculetteException
+	{
+		try
+		{
+			calc.setAcc(Float.parseFloat(s));
+		}
+		catch (NumberFormatException e)
+		{
+			throw new CalculetteException("Entrez un nombre svp !");
+		}
+		
+		return(3); // passe dans l'etat 3
+	}
+}
