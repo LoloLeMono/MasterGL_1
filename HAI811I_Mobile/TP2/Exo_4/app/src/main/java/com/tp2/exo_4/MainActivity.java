44c4daac2b3fe0a69f6b8d049c6a5024d5764bed
LoloLeMono <rouquairolr@gmail.com>
2023-03-02 16:48:46 +0100

    Fix exo_4

--- HAI811I_Mobile/TP2/Exo_4/app/src/main/java/com/tp2/exo_4/MainActivity.java
+++ HAI811I_Mobile/TP2/Exo_4/app/src/main/java/com/tp2/exo_4/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -17,11 +19,10 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity{
 
-    SensorManager sensorMgr;
-    Sensor dirSensor;
     ImageView arrow;
+    float x_prev, y_prev, z_prev;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,100 +30,58 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
 
         arrow = findViewById(R.id.arrow);
+        TextView pos = findViewById(R.id.pos);
 
-        sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
-        dirSensor = sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        float biais = (float)0.7;
 
-        Boolean supported = sensorMgr.registerListener((SensorEventListener) this,
-                dirSensor, SensorManager.SENSOR_DELAY_UI);
-        sensorMgr.unregisterListener
-                ((SensorEventListener) this, dirSensor);
-        if (!supported) {
-            arrow.setVisibility(View.INVISIBLE);
-        }
-    }
+        // Obtention du gestionnaire de capteurs
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        sensorMgr.registerListener((SensorEventListener) this, dirSensor, SensorManager.SENSOR_DELAY_NORMAL);
-    }
+        // Obtention de l'accéléromètre
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-    @Override
-    protected void onPause() {
-        sensorMgr.unregisterListener(this, dirSensor);
-        super.onPause();
-    }
+        // Enregistrement de l'écouteur de capteurs
+        sensorManager.registerListener(new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                float x = event.values[0];
+                float y = event.values[1];
 
-    private static final float NS2S = 1.0f / 1000000000.0f; // Convert nanoseconds to seconds
-    private static final float EPSILON = 0.000001f; // Epsilon value to avoid division by zero
-    private long previousTimestamp;
-    private float[] previousPosition = new float[3];
-    private float[] currentVelocity = new float[3];
-    private float[] currentAcceleration = new float[3];
-    private float[] rotationMatrix = new float[9];
-    private float[] orientation = new float[3];
-    private float[] gravity = new float[3];
+                pos.setText("x : " + x + ", y : " + y);
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        switch (event.sensor.getType()) {
-            case Sensor.TYPE_ACCELEROMETER:
-                System.arraycopy(event.values, 0, currentAcceleration, 0, 3);
-                break;
-            case Sensor.TYPE_GRAVITY:
-                System.arraycopy(event.values, 0, gravity, 0, 3);
-                break;
-            default:
-                return;
-        }
+                // x positif = gauche
+                // x negatif = droite
+                // y positif = haut
+                // y negatif = bas
 
-        float dt = (event.timestamp - previousTimestamp) * NS2S;
-        previousTimestamp = event.timestamp;
-
-        // Remove gravity from the acceleration values
-        float alpha = 0.8f;
-        float[] smoothedAcceleration = new float[3];
-        smoothedAcceleration[0] = alpha * currentAcceleration[0] + (1 - alpha) * gravity[0];
-        smoothedAcceleration[1] = alpha * currentAcceleration[1] + (1 - alpha) * gravity[1];
-        smoothedAcceleration[2] = alpha * currentAcceleration[2] + (1 - alpha) * gravity[2];
-
-        // Convert the smoothed acceleration to the device coordinate system
-        SensorManager.getRotationMatrix(rotationMatrix, null, gravity, event.values);
-        SensorManager.getOrientation(rotationMatrix, orientation);
-        float[] deviceAcceleration = new float[3];
-        deviceAcceleration[0] = smoothedAcceleration[0] * (float)Math.cos(orientation[1]) * (float)Math.cos(orientation[2]);
-        deviceAcceleration[1] = smoothedAcceleration[1] * (float)Math.sin(orientation[2]);
-        deviceAcceleration[2] = smoothedAcceleration[2] * (float)Math.sin(orientation[1]) * (float)Math.cos(orientation[2]);
-
-        // Integrate the acceleration to get the velocity
-        for (int i = 0; i < 3; i++) {
-            currentVelocity[i] += (currentAcceleration[i] + deviceAcceleration[i]) / 2.0f * dt;
-        }
-
-        // Integrate the velocity to get the position
-        for (int i = 0; i < 3; i++) {
-            if (i == 0){
-                if (previousPosition[0] + currentVelocity[0] < previousPosition[0]){
+                if (x > x_prev+biais) {
                     arrow.setImageResource(R.drawable.arrow_left);
-                } else if (previousPosition[0] + currentVelocity[0] > previousPosition[0]) {
+                } else if (x < x_prev-biais){
                     arrow.setImageResource(R.drawable.arrow_right);
+                } else if (y > y_prev+biais) {
+                    arrow.setImageResource(R.drawable.arrow_top);
+                } else if (y < y_prev-biais) {
+                    arrow.setImageResource(R.drawable.arrow_bottom);
                 }
-            } else if (i == 1) {
-                if (currentVelocity[1] < 0) {
-                    //arrow.setImageResource(R.drawable.arrow_left);
-                } else if (currentVelocity[1] < 0) {
-                    //arrow.setImageResource(R.drawable.arrow_left);
-                }
-            }
-            previousPosition[i] += currentVelocity[i] * dt;
-        }
 
-        // Do something with the position, such as update a UI element
-        Log.d("MySensorEventListener", "xPosition = " + previousPosition[0] + ", yPosition = " + previousPosition[1] + ", zPosition = " + previousPosition[2]);
-    }
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
+                /*
+                if (y + 0.2 > y_prev) {
+                    arrow.setImageResource(R.drawable.arrow_top);
+                } else if (y + 0.2 < y_prev) {
+                    arrow.setImageResource(R.drawable.arrow_bottom);
+                }
+
+                 */
+
+                x_prev = x;
+                y_prev = y;
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                // Ne fait rien
+            }
+        }, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
     }
 }
