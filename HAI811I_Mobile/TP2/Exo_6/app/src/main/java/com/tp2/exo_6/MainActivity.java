94f81c63f466bd8994d5a52285df49ec2d844f27
LoloLeMono <rouquairolr@gmail.com>
2023-03-03 11:32:50 +0100

    Fix exo_6

--- HAI811I_Mobile/TP2/Exo_6/app/src/main/java/com/tp2/exo_6/MainActivity.java
+++ HAI811I_Mobile/TP2/Exo_6/app/src/main/java/com/tp2/exo_6/MainActivity.java
@@ -7,16 +7,20 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     SensorManager sensorMgr;
     Sensor proxSensor;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img = findViewById(R.id.imgThing);
 
         sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
         proxSensor = sensorMgr.getDefaultSensor(Sensor.TYPE_PROXIMITY);
@@ -41,7 +45,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private void onProximityChanged(SensorEvent event){
         float distance;
         distance = event.values[0];
-        ((TextView)findViewById(R.id.prox)).setText(distance +
-                " cm");
+        ((TextView)findViewById(R.id.prox)).setText(distance + " cm");
+
+        if (distance < 3){
+            img.setImageResource(R.drawable.loupe);
+        } else if (distance > 3 && distance < 6){
+            img.setImageResource(R.drawable.binoculars);
+        } else{
+            img.setImageResource(R.drawable.telescope);
+        }
     }
 }
